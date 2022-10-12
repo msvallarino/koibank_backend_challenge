3766c56a5b8af6be0fed5178c40a54c0ea0d0d0a
mvallarino <matias.vallarino@globant.com>
2022-10-11 22:15:26 -0500

    feat: add db data access layers

---
+++ src/db/dal/User.ts
@@ -0,0 +1,18 @@
+import User, { IUser } from '../models/User';
+
+export const create = async (newUser: IUser): Promise<IUser> => {
+  const user = new User(newUser);
+  await User.create(user);
+  return user;
+};
+
+export const getByName = async (name: string): Promise<IUser | undefined> => {
+  const query = await User.find({ username: name }).exec();
+
+  return query[0];
+};
+
+export const verifyPass = async (userToVerify: IUser, password: string): Promise<boolean> => {
+  const user = new User(userToVerify);
+  return user.verifyPassword(password);
+};
